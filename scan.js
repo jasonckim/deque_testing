e0ea617a4d8f2fce92c0e5f160ab5752867122cc
Jason Kim <jasonkim@Jasons-MBP-2.attlocal.net>
2022-10-13 00:09:39 -0700

    new changes to asserts

--- scan.js
+++ scan.js
@@ -1,17 +1,74 @@
 const AxeBuilder = require('@axe-core/webdriverjs');
-const WebDriver = require('selenium-webdriver');
+const { Builder, By, Key } = require("selenium-webdriver");
 const assert = require("chai").assert;
 
-const driver = new WebDriver.Builder().forBrowser('chrome').build();
+describe("verify: nav has loaded, page title is correct, nav links work, and page analyzed", function () {
+  it("main-nav element has loaded", async function () {
+    let driver = await new Builder().forBrowser('chrome').build();
 
-driver.get('https://dequeuniversity.com/demo/mars/').then(() => {
-  new AxeBuilder(driver).analyze().then(results => {
-    if (results.violations.length == 0) {
-      assert.equal(0, true, "no violations found")
-    }
-    else {
-      console.log(results.violations.length)
-      console.log(results.violations)
-    }
+    try {
+       await driver.get("https://dequeuniversity.com/demo/mars/");
+
+       let check = await driver
+         .findElement(By.id('main-nav'))
+         .isDisplayed();
+
+       assert.equal(check, true);
+     } finally {
+       console.log("FIRST TEST PASSED");
+        await driver.quit();
+     }
   })
-});
+
+  it("page title is correct", async function () {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+       await driver.get("https://dequeuniversity.com/demo/mars/");
+
+      let titleCheck = await driver.getTitle();
+
+       assert.equal(titleCheck, 'Mars Commuter: Travel to Mars for Work or Pleasure!');
+     } finally {
+       console.log("SECOND TEST PASSED");
+       await driver.quit();
+     }
+  })
+
+  it("links redirect to correct page", async function () {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+       await driver.get("https://dequeuniversity.com/demo/mars/");
+
+      let linkCheck = await driver
+        .findElement(By.tagName('a'))
+        .click()
+        return driver.getCurrentUrl();
+
+       assert.equal(linkCheck, 'https://dequeuniversity.com/demo/mars/mars2.html?a=send_me_to_mars');
+     } finally {
+       console.log("THIRD TEST PASSED");
+     }
+  })
+
+  it("analyzes the page with axe", async function () {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try{
+      await driver.get('https://dequeuniversity.com/demo/mars/').then(() => {
+        new AxeBuilder(driver).analyze().then(results => {
+          if (results.violations.length == 0) {
+            assert.equal(0, true, "no violations found")
+          }
+          else {
+            console.log("Acessibility Violations:", results.violations.length)
+          }
+        })
+      })
+    } finally{
+      console.log("TESTS COMPLETED")
+    }
+
+  })
+})
